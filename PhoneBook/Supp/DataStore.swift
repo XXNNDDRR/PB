759abb259a9cdf6a1321d8773b033f657ea6d57d
XXNNDDRR <xndrgo@gmail.com>
2023-12-16 17:24:21 +0700

    Add

---
+++ PhoneBook/Supp/DataStore.swift
@@ -0,0 +1,16 @@
+//
+//  DataStore.swift
+//  PhoneBook
+//
+//  Created by Alexander on 16.12.2023.
+//
+
+import Foundation
+
+final class DataStore {
+    
+    var names = ["Alexander", "Oleg", "Serge", "George", "Alex"]
+    var surnames = ["Silver", "Moon", "Gold", "Doe", "Kuzmin"]
+    var phones = ["555 - 1234", "555 - 2244", "555 - 0000", "555 - 6600", "555 - GODE"]
+    var emails = ["silver@mail.com", "moon@mail.com", "gold@mail.com", "doe@mail.com", "kuzmin@mail.com"]
+}
